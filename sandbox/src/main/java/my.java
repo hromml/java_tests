96ca6420a205d7a117a8ad35c9eeb1b72a333b71
hromml <mrkhramkov@gmail.com>
2017-02-25 12:43:59 +0400

    packet

--- sandbox/src/main/java/my.java
+++
@@ -1,5 +0,0 @@
-public class my{
-	public static void main(String[] args) {
-		System.out.println("Hello world");
-	}
-}
---
+++ sandbox/src/main/java/ru/hromml/test/sandbox/MyFirstProgram.java
@@ -0,0 +1,13 @@
+package ru.hromml.test.sandbox;
+
+public class MyFirstProgram {
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		System.out.println(2+2);
+		System.out.println(2*2);
+		System.out.println(2/2);
+		System.out.println(2-2);
+		System.out.println(1.0/2.0);
+		System.out.println("2"+"3");
+	}
+}
