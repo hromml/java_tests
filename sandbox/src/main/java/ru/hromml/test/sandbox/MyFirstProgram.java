9eb809cd25df6a2dc444ba2e978adb1654ede905
hromml <mrkhramkov@gmail.com>
2017-02-25 13:04:33 +0400

    ploshad' kvadrata

--- sandbox/src/main/java/ru/hromml/test/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/hromml/test/sandbox/MyFirstProgram.java
@@ -3,11 +3,8 @@ package ru.hromml.test.sandbox;
 public class MyFirstProgram {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
-		System.out.println(2+2);
-		System.out.println(2*2);
-		System.out.println(2/2);
-		System.out.println(2-2);
-		System.out.println(1.0/2.0);
-		System.out.println("2"+"3");
+		int x=5;
+		System.out.println("Plosh kv so storonoi "+x+" = "+(x*x));
+
 	}
 }
