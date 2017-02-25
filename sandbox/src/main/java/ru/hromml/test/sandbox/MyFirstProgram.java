c681d1e149f83922f077513a3d6fdb2099e99027
hromml <mrkhramkov@gmail.com>
2017-02-25 13:15:17 +0400

    readding to github

--- sandbox/src/main/java/ru/hromml/test/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/hromml/test/sandbox/MyFirstProgram.java
@@ -3,7 +3,7 @@ package ru.hromml.test.sandbox;
 public class MyFirstProgram {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
-		int x=5;
+		int x=6;
 		System.out.println("Plosh kv so storonoi "+x+" = "+(x*x));
 
 	}
