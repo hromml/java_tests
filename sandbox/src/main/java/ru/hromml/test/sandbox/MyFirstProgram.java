f78472b4097bcf2c0500f6c17522055868c19bd5
hromml <mrkhramkov@gmail.com>
2017-02-25 14:05:50 +0400

    Code rollback

--- sandbox/src/main/java/ru/hromml/test/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/hromml/test/sandbox/MyFirstProgram.java
@@ -2,11 +2,6 @@ package ru.hromml.test.sandbox;
 
 public class MyFirstProgram {
 	public static void main(String[] args) {
-		String newstring = "Hello, world!";
-		System.out.println(newstring);
-		double x=6;
-		double y=x*x;
-		System.out.println("Plosh kv so storonoi "+x+" = "+y);
-
-	}
+		System.out.println("Hello, world!");
+			}
 }
