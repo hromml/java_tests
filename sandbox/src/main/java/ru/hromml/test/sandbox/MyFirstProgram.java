ddc6c4669981fd6eaf225cde946d5eedaffe38da
hromml <mrkhramkov@gmail.com>
2017-02-25 13:27:50 +0400

    types

--- sandbox/src/main/java/ru/hromml/test/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/hromml/test/sandbox/MyFirstProgram.java
@@ -2,9 +2,10 @@ package ru.hromml.test.sandbox;
 
 public class MyFirstProgram {
 	public static void main(String[] args) {
-		System.out.println("Hello world");
-		int x=6;
-		int y=x*x;
+		String newstring = "Hello, world!";
+		System.out.println(newstring);
+		double x=6;
+		double y=x*x;
 		System.out.println("Plosh kv so storonoi "+x+" = "+y);
 
 	}
