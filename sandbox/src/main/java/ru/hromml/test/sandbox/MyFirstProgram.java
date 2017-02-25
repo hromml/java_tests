e23f88432b599488b969e7992cb300f19de69591
hromml <mrkhramkov@gmail.com>
2017-02-25 14:27:01 +0400

    two areas

--- sandbox/src/main/java/ru/hromml/test/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/hromml/test/sandbox/MyFirstProgram.java
@@ -2,6 +2,20 @@ package ru.hromml.test.sandbox;
 
 public class MyFirstProgram {
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
-			}
+		hello("world");
+		hello("sas");
+		hello("wow");
+		double length = 5;
+		double length2 = 6;
+		System.out.println("ploshad = "+area(length)+" and "+area(length,length2));
+	}
+	public static void hello(String newstring){
+		System.out.println("Hello, "+newstring);
+	}
+	public static double area(double l){
+		return l*l;
+	}
+	public static double area(double l1,double l2){
+		return l1*l2;
+	}
 }
