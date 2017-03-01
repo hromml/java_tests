40b62bc524bc571d3c2838d4d026385d5ec43970
hromml <mrkhramkov@gmail.com>
2017-03-01 19:37:45 +0400

    remove "login" code to the method "BeforeMethod"

--- addbook_web_tests/src/test/java/ru/hromml/test/addbook/GroupCreationTest.java
+++ addbook_web_tests/src/test/java/ru/hromml/test/addbook/GroupCreationTest.java
@@ -22,10 +22,6 @@ public class GroupCreationTest {
         System.setProperty("webdriver.gecko.driver","C:\\Games\\geckodriver.exe");
         wd = new FirefoxDriver();
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-    }
-    
-    @Test
-    public void GroupCreationTest() {
         wd.get("http://localhost/addressbook/group.php");
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
@@ -35,6 +31,11 @@ public class GroupCreationTest {
         wd.findElement(By.name("pass")).clear();
         wd.findElement(By.name("pass")).sendKeys("secret");
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    }
+    
+    @Test
+    public void testGroupCreation() {
+
         wd.findElement(By.name("new")).click();
         wd.findElement(By.name("group_name")).click();
         wd.findElement(By.name("group_name")).clear();
